3b3fcad07873467f54c58f2b63277898d98ccf8e
natethemasterman <nathan.moran25@ycdsbk12.ca>
2024-04-23 07:06:47 -0400

    a

--- Sketch.java
+++ Sketch.java
@@ -1,38 +1,102 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-    size(400, 400);
-  }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+  // Setup code with background size and color
+    size(400, 400);
+    background(0, 191, 255);
   }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+	
+  // Calling parameters
   public void draw() {
-    
+    drawFlower(40, 80, 25);
+    drawFlower(80, 80, 25);
+    drawFlower(120, 80, 25);
+    drawFlower(160, 80, 25);
+    drawFlower(200, 80, 25);
+    drawFlower(240, 80, 25);
+    drawFlower(280, 80, 25);
+    drawFlower(320, 80, 25);
+    drawFlower(360, 80, 25);
+    drawFlower(40, 160, 25);
+    drawFlower(80, 160, 25);
+    drawFlower(120, 160, 25);
+    drawFlower(160, 160, 25);
+    drawFlower(200, 160, 25);
+    drawFlower(240, 160, 25);
+    drawFlower(280, 160, 25);
+    drawFlower(320, 160, 25);
+    drawFlower(360, 160, 25);
+    drawFlower(40, 240, 25);
+    drawFlower(80, 240, 25);
+    drawFlower(120, 240, 25);
+    drawFlower(160, 240, 25);
+    drawFlower(200, 240, 25);
+    drawFlower(240, 240, 25);
+    drawFlower(280, 240, 25);
+    drawFlower(320, 240, 25);
+    drawFlower(360, 240, 25);
+    drawFlower(40, 320, 25);
+    drawFlower(80, 320, 25);
+    drawFlower(120, 320, 25);
+    drawFlower(160, 320, 25);
+    drawFlower(200, 320, 25);
+    drawFlower(240, 320, 25);
+    drawFlower(280, 320, 25);
+    drawFlower(320, 320, 25);
+    drawFlower(360, 320, 25);
+    drawFace();
   }
 
-  /**
-   * Description
-   * 
-   * @param 
-   * @param 
-   * @return
-   * @author 
-   */
+  // Draw Flower
+  void drawFlower(float flowerX, float flowerY, float flowerSize) {
+    float flowerDistance = flowerSize / 2;
 
-  
-  
+  fill(255, 105, 180);
+
+  // Middle part
+  fill(59, 0, 0);
+  ellipse(flowerX, flowerY, 
+    flowerSize, flowerSize);
+
+  // Upper-left part
+  ellipse(flowerX - flowerDistance, flowerY - flowerDistance, 
+    flowerSize, flowerSize);
+
+  // Upper-right part
+  ellipse(flowerX + flowerDistance, flowerY - flowerDistance, 
+    flowerSize, flowerSize);
+
+  // Lower-left part
+  ellipse(flowerX - flowerDistance, flowerY + flowerDistance, 
+    flowerSize, flowerSize);
+
+  // Lower-right part
+  ellipse(flowerX + flowerDistance, flowerY + flowerDistance, 
+    flowerSize, flowerSize);
+  }
+
+  // Draw Face
+  void drawFace() {
+  // Head
+  fill(34, 139, 34);
+  ellipse(100, 100, 150, 150);
+
+  // Eyes
+  fill(255);
+  ellipse(75, 85, 30, 20);
+  ellipse(125, 85, 30, 20);
+
+  // Pupils
+  fill(0);
+  ellipse(75, 85, 10, 10);
+  ellipse(125, 85, 10, 10);
+
+  // Mouth
+  fill(255, 0, 0);
+  arc(100, 125, 80, 50, 0, PI);
+  line(60, 125, 140, 125);
+  }
 }
+
